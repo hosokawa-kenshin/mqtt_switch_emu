8559eac03a065cb3df371faa84f635cdccfed479
hosokawa-kenshin <pnl84lku@s.okayama-u.ac.jp>
2023-05-25 11:31:49 +0900

    Add publish function when update state

--- switch.c
+++ switch.c
@@ -4,10 +4,13 @@
 #include <stdbool.h>
 #include <unistd.h>
 #include <time.h>
-// MQTTライブラリのヘッダファイルをインクルードする
 #include <mosquitto.h>
 
 #define MAX_PAYLOAD_SIZE 256
+//QUALITY_OF_SERVICE メッセージに使用されるサービス品質を示す整数値 0，1，2
+#define QUALITY_OF_SERVICE 1
+//RETAIN メッセージを保持に関するbool
+#define RETAIN true
 
 #ifndef TRUE
 #define TRUE 1
@@ -17,62 +20,65 @@
 #define FALSE 0
 #endif
 
-char *topic   = "openHAB/bot/switch1";
-char *message = NULL;
-int state     = 0;
-int connect_desire = TRUE;
-
+char *sub_topic   = "bot/switch";
+char *pub_topic   = "openhab/bot/switch";
+int state         = 0;
 
 //Brokerとの接続成功時に実行されるcallback関数
-void on_connect(struct mosquitto *mosq, void *obj, int result)
-{
-    mosquitto_subscribe(mosq, NULL, topic, 0);
+void on_connect(struct mosquitto *mosq, void *obj, int result){
+  mosquitto_subscribe(mosq, NULL, sub_topic, RETAIN);
 }
 
-void on_message(struct mosquitto *mosq, void *obj, const struct mosquitto_message *message)
-{
-    if(message->payloadlen){
-        printf("%s ", message->topic);
-        if (strcmp(message->payload,"ON")){
-            state = 0;
-        }else{
-            state = 1;
-        }
-        fwrite(message->payload, 1, message->payloadlen, stdout);
-        printf(" state:%d\n",state);
+//Brokerからのメッセージ受信時に実行されるcallback関数
+void on_message(struct mosquitto *mosq, void *obj, const struct mosquitto_message *message){
+  char *pub_message = (char *)malloc(sizeof(char) * MAX_PAYLOAD_SIZE);
+  if(message->payloadlen){
+    if(strcmp(message->payload,"ON")){
+      state = 0;
+      strncpy(pub_message,"OFF",strlen("OFF") + 1);
+      mosquitto_publish(mosq, NULL, pub_topic, strlen(pub_message), pub_message, QUALITY_OF_SERVICE, RETAIN);
     }else{
-        printf("%s (null)\n", message->topic);
+      state = 1;
+      strncpy(pub_message,"ON",strlen("ON") + 1);
+      mosquitto_publish(mosq, NULL, pub_topic, strlen(pub_message), pub_message, QUALITY_OF_SERVICE, RETAIN);
     }
-
-    fflush(stdout);
+    printf("sub_topic: %s\nsub_message: ", message->topic);
+    fwrite(message->payload, 1, message->payloadlen, stdout);
+    printf("\npub_topic: %s\npub_message: %s\nstate: %d\n\n", pub_topic, pub_message, state);
+  }else{
+    printf("%s (null)\n", message->topic);
+  }
+  fflush(stdout);
+  free(pub_message);
 }
 
 int main() {
-    char *id            = "bot/switch";
-    char *host          = "localhost";
-    int   port          = 1883;
-    int   keepalive     = 60;
-    bool clean_session  = true;
-    struct mosquitto *mosq = NULL;
-    
-    mosquitto_lib_init();
-    mosq = mosquitto_new(id, clean_session, NULL);
-    if(!mosq){
-        fprintf(stderr, "Cannot create mosquitto object\n");
-        mosquitto_lib_cleanup();
-        return(EXIT_FAILURE);
-    }
+  char *id            = "switch1";
+  char *host          = "localhost";
+  int   port          = 1883;
+  int   keepalive     = 60;
+  bool clean_session  = true;
+  struct mosquitto *mosq = NULL;
+  
+  mosquitto_lib_init();
+  mosq = mosquitto_new(id, clean_session, NULL);
 
-    mosquitto_connect_callback_set(mosq, on_connect);
-    mosquitto_message_callback_set(mosq, on_message);
+  if(!mosq){
+    fprintf(stderr, "Cannot create mosquitto object\n");
+    mosquitto_lib_cleanup();
+    return(EXIT_FAILURE);
+  }
 
-    if(mosquitto_connect_bind(mosq, host, port, keepalive, NULL)){
-        fprintf(stderr, "failed to connect broker.\n");
-        mosquitto_lib_cleanup();
-        return(EXIT_FAILURE);
-    }
+  mosquitto_connect_callback_set(mosq, on_connect);
+  mosquitto_message_callback_set(mosq, on_message);
 
-    mosquitto_loop_forever(mosq, -1, 1);
+  if(mosquitto_connect_bind(mosq, host, port, keepalive, NULL)){
+    fprintf(stderr, "failed to connect broker.\n");
+    mosquitto_lib_cleanup();
+    return(EXIT_FAILURE);
+  }
 
-    return(EXIT_SUCCESS);
+  mosquitto_loop_forever(mosq, -1, 1);
+
+  return(EXIT_SUCCESS);
 }
